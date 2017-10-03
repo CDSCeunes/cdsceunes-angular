4473c59a41aa0978c8d5a554db45eabaeaed28ca
emethium <nikolas.serafini@gmail.com>
2017-10-02 21:57:13 -0300

    Position's form now loads commission data from API

--- src/app/positions/positions-form/positions-form.component.ts
+++ src/app/positions/positions-form/positions-form.component.ts
@@ -1,24 +1,41 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { Location } from '@angular/common';
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
 
 import { PositionService } from '../../_services/position.service';
 import { Position } from '../../_models/position';
 
+import { Commission } from '../../_models/commission';
+import { CommissionSearchService } from '../../_services/commission-search.service';
+
+// Observable class extensions
+import 'rxjs/add/observable/of';
+
+// Observable operators
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
 
 @Component({
   selector: 'app-position-form',
   templateUrl: './positions-form.component.html',
-  styleUrls: [ './positions-form.component.css' ]
+  styleUrls: [ './positions-form.component.css' ],
+  providers: [CommissionSearchService]
 })
 
 export class PositionsFormComponent {
   position = new Position();
+  commissions: Observable<Commission[]>;
+  private searchTerms = new Subject<string>();
 
   constructor (
     private positionService: PositionService,
     private route: ActivatedRoute,
-    private location: Location
+    private location: Location,
+    private commissionSearchService: CommissionSearchService,
+    private router: Router
   ) {}
 
   submitted = false;
@@ -35,5 +52,30 @@ export class PositionsFormComponent {
 
   goBack(): void {
     this.location.back();
+  }
+
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
+
+  selectCommission(content: string): void {
+    this.position.commission = content;
+    this.searchTerms.next('');
+  }
+
+  ngOnInit(): void {
+    this.commissions = this.searchTerms
+      .debounceTime(300)        // wait 300ms after each keystroke before considering the term
+      .distinctUntilChanged()   // ignore if next search term is same as previous
+      .switchMap(term => term   // switch to new observable each time the term changes
+        // return the http search observable
+        ? this.commissionSearchService.search(term)
+        // or the observable of empty commissiones if there was no search term
+        : Observable.of<Commission[]>([]))
+      .catch(error => {
+        // TODO: add real error handling
+        console.log(error);
+        return Observable.of<Commission[]>([]);
+      });
   }
 }
