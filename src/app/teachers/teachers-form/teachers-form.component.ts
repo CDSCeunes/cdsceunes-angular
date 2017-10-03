5665b19a6de236525ff67b52f90bf6e8fe420ecb
emethium <nikolas.serafini@gmail.com>
2017-10-02 21:04:26 -0300

    Teacher's forms now search for stored departments

--- src/app/teachers/teachers-form/teachers-form.component.ts
+++ src/app/teachers/teachers-form/teachers-form.component.ts
@@ -1,33 +1,50 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { Location } from '@angular/common';
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
 
 import { TeacherService } from '../../_services/teacher.service';
 import { Teacher } from '../../_models/teacher';
 
+import { Department } from '../../_models/department';
+import { DepartmentSearchService } from '../../_services/department-search.service';
+
+// Observable class extensions
+import 'rxjs/add/observable/of';
+
+// Observable operators
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
 
 @Component({
   selector: 'app-teacher-form',
   templateUrl: './teachers-form.component.html',
-  styleUrls: [ './teachers-form.component.css' ]
+  styleUrls: ['./teachers-form.component.css'],
+  providers: [DepartmentSearchService]
 })
 
 export class TeachersFormComponent {
   teacher = new Teacher();
+  departments: Observable<Department[]>;
+  private searchTerms = new Subject<string>();
 
-  constructor (
+  constructor(
     private teacherService: TeacherService,
     private route: ActivatedRoute,
-    private location: Location
-  ) {}
+    private location: Location,
+    private departmentSearchService: DepartmentSearchService,
+    private router: Router
+  ) { }
 
   submitted = false;
 
   onSubmit() {
-      this.submitted = true;
-      this.teacherService.createComplete(
-        this.teacher.name, this.teacher.department, this.teacher.email, this.teacher.center)
-        .then(() => this.goBack());
+    this.submitted = true;
+    this.teacherService.createComplete(
+      this.teacher.name, this.teacher.department, this.teacher.email, this.teacher.center)
+      .then(() => this.goBack());
   }
 
   newTeacher() {
@@ -37,4 +54,30 @@ export class TeachersFormComponent {
   goBack(): void {
     this.location.back();
   }
+
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
+
+  selectDepartment(content: string): void {
+    this.teacher.department = content;
+    this.searchTerms.next('');
+  }
+
+  ngOnInit(): void {
+    this.departments = this.searchTerms
+      .debounceTime(300)        // wait 300ms after each keystroke before considering the term
+      .distinctUntilChanged()   // ignore if next search term is same as previous
+      .switchMap(term => term   // switch to new observable each time the term changes
+        // return the http search observable
+        ? this.departmentSearchService.search(term)
+        // or the observable of empty departmentes if there was no search term
+        : Observable.of<Department[]>([]))
+      .catch(error => {
+        // TODO: add real error handling
+        console.log(error);
+        return Observable.of<Department[]>([]);
+      });
+  }
+
 }
